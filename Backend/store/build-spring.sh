dd0ceb90d675b65a03d3089744e10f944e830ed2
Akash-Sharma1 <aakashsharmajc100@gmail.com>
2021-01-20 14:40:32 +0530

    dockerised spring

--- Backend/store/build-spring.sh
+++ Backend/store/build-spring.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-mvn clean install
-mvn spring-boot:run
+docker run --name mysql-standalone -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=db_store -e MYSQL_USER=akashsharma -e MYSQL_PASSWORD=password -d mysql:5.6
+
+docker build . -t store-web-app-backend
+
+docker run -p 8080:8080 --name store-web-app-backend --link mysql-standalone:mysql -d store-web-app-backend
