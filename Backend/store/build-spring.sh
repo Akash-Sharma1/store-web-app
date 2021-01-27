415c396ad7685f4848b5e48a3c43c7707e14bd8e
Akash-Sharma1 <aakashsharmajc100@gmail.com>
2021-01-27 23:10:52 +0530

    staging exp 3

--- Backend/store/build-spring.sh
+++ Backend/store/build-spring.sh
@@ -4,10 +4,3 @@ docker run --name mysql-standalone -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATA
 docker build . -t store-web-app-backend
 
 docker run -p 8080:8080 --name store-web-app-backend --link mysql-standalone:mysql -d store-web-app-backend
-
-docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-
-docker tag store-web-app-backend:latest $DOCKER_USERNAME/store-web-app-backend:latest
-
-docker push $DOCKER_USERNAME/store-web-app-backend:latest
-
